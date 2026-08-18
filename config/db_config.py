ac8c7384a8188c05bac81039914ef8b229e8c4ea
ziyasha <168514170+ziyasha@users.noreply.github.com>
2026-08-18 10:07:39 +0000

    Secure database configuration and ignore local files

--- config/db_config.py
+++ config/db_config.py
@@ -1,6 +1,8 @@
 # config/db_config.py
 
-HOST = "localhost"
-USER = "root"
-PASSWORD = "root"
-DATABASE = "banking_system"
+import os
+
+HOST = os.getenv("DB_HOST", "127.0.0.1")
+USER = os.getenv("DB_USER", "bankapp")
+PASSWORD = os.getenv("DB_PASSWORD", "")
+DATABASE = os.getenv("DB_NAME", "banking_system")
